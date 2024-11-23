b5a8808041cc47d221b6c29aa1a5bc04c851d4c4
ziad-mostafa <ziadmostafa760@gmail.com>
2024-11-23 11:04:04 +0200

    small change make when i press o while iam in comment line the next line write with no comment from prev line

--- init.lua
+++ init.lua
@@ -3,3 +3,13 @@ require("ziad")
 vim.opt.relativenumber = true
 vim.opt.number = true
 
+
+
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "r", "o" })
+  end,
+})
+
