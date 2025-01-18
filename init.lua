2cc73c22975847b7a3ac4abc15f46cdbdc5da8d6
ziad_mostafa <ziadmostafa760@gmail.com>
2025-01-18 23:52:43 +0200

    new statusbar or whatever its cool

--- init.lua
+++ init.lua
@@ -179,6 +179,38 @@ require("lazy").setup({
     end,
   },
 
+
+ -- Lualine (statusline)
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "tokyonight",
+          icons_enabled = true,
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+      })
+    end,
+  },
+
+
+
+
+
+
+
+
   -- Harpoon
   {
     "ThePrimeagen/harpoon",
